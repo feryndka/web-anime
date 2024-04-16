20a7eb79b5bf102f22a8cdfa41cd7ada7ec14405
feryndka <feryandika151@gmail.com>
2024-04-17 00:45:37 +0700

    fix:fetch limit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,10 @@ import Header from "@/components/ListAnime/Header";
 import { GetAnime, GetNestedAnime, reproduce } from "../libs/api-libs";
 
 export default async function Home() {
-  const topAnime = await GetAnime("top/anime", "limit=15");
+  // const topAnime = await GetAnime("top/anime", "limit=10");
+  const res = await fetch("https://api.jikan.moe/v4/top/anime?limit=10");
+  const topAnime = await res.json();
+  
   let recommenAnime = await GetNestedAnime("recommendations/anime", "entry");
   recommenAnime = reproduce(recommenAnime, 5);
 
